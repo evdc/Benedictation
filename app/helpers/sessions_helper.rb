3bc5374ce5f06b4f38cdecdaefe8db191a4447b3
Evan Crook <evannc@gmail.com>
2015-04-01 00:28:59 -0700

    User-online indicator for chat page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,40 +5,41 @@ module SessionsHelper
 	end
 
 	def log_in(user)
+        user.log_in
     	session[:user_id] = user.id
-  end
-
-  def create_state
-    state = get_random_string
-    session[:state] = state
-  end
-
-  def log_out
-		session.delete(:user_id)
-		@current_user = nil
-	end
-
-	def current_user?(user)
-    	user == current_user
-	end
-
-  def redirect_back_or(default)
-    redirect_to(session[:forwarding_url] || default)
-    session.delete(:forwarding_url)
-  end
-
-  def store_location
-    session[:forwarding_url] = request.url if request.get?
-  end
-
-  def redirect
-    if !logged_in?
-        store_location
-        redirect_to root_url
-        flash[:danger] = "Please log in."
-    else
     end
-  end
 
+    def create_state
+        state = get_random_string
+        session[:state] = state
+    end
+
+    def log_out
+        current_user.log_out
+        session.delete(:user_id)
+        @current_user = nil
+    end
+
+    def current_user?(user)
+        user == current_user
+    end
+
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    def store_location
+    session[:forwarding_url] = request.url if request.get?
+    end
+
+    def redirect
+        if !logged_in?
+            store_location
+            redirect_to root_url
+            flash[:danger] = "Please log in."
+        else
+        end
+    end
 
 end
